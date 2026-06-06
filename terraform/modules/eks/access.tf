88b2347b12509228c8570f023424981274ab6d28
Param2ray <param_2ray@hotmail.co.uk>
2026-06-05 20:18:09 -0400

    Fix EKS access entries

--- terraform/modules/eks/access.tf
+++ terraform/modules/eks/access.tf
@@ -1,4 +1,28 @@
-resource "aws_eks_access_entry" "github_actions_platform_admin" {
+resource "aws_eks_access_entry" "param_admin" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = "arn:aws:iam::512378127667:user/Param"
+  type          = "STANDARD"
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
+}
+
+resource "aws_eks_access_policy_association" "param_admin" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = aws_eks_access_entry.param_admin.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [
+    aws_eks_access_entry.param_admin
+  ]
+}
+
+resource "aws_eks_access_entry" "github_actions_admin" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
   principal_arn = "arn:aws:iam::512378127667:role/github-actions-eks-platform-role"
   type          = "STANDARD"
@@ -8,9 +32,9 @@ resource "aws_eks_access_entry" "github_actions_platform_admin" {
   ]
 }
 
-resource "aws_eks_access_policy_association" "github_actions_platform_admin" {
+resource "aws_eks_access_policy_association" "github_actions_admin" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = aws_eks_access_entry.github_actions_platform_admin.principal_arn
+  principal_arn = aws_eks_access_entry.github_actions_admin.principal_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
@@ -18,7 +42,7 @@ resource "aws_eks_access_policy_association" "github_actions_platform_admin" {
   }
 
   depends_on = [
-    aws_eks_access_entry.github_actions_platform_admin
+    aws_eks_access_entry.github_actions_admin
   ]
 }
 
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -173,27 +173,3 @@ resource "aws_eks_node_group" "eks_node_group" {
     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-
-resource "aws_eks_access_entry" "github_actions_admin" {
-  cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = "arn:aws:iam::512378127667:role/github-actions-eks-platform-role"
-  type          = "STANDARD"
-
-  depends_on = [
-    aws_eks_cluster.eks_cluster
-  ]
-}
-
-resource "aws_eks_access_policy_association" "github_actions_admin" {
-  cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = aws_eks_access_entry.github_actions_admin.principal_arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-  access_scope {
-    type = "cluster"
-  }
-
-  depends_on = [
-    aws_eks_access_entry.github_actions_admin
-  ]
-}
